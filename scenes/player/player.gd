370576bfbaa0c4bc53218b1032ceb3b4442c40bf
ChristiannYa <christian.ylopez02@gmail.com>
2026-08-26 19:18:42 -0400

    10-player-model

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,6 +1,10 @@
 class_name Player
 extends CharacterBody3D
 
+
+@onready var animation_player: AnimationPlayer = $"character-female-a2/AnimationPlayer"
+
+
 const _JUMP_FORCE = 20.0
 const _GRAVITY = 40.0
 
@@ -10,9 +14,14 @@ func _ready():
 
 func _physics_process(delta: float):
 	handle_gravity(delta)
+	handle_animation()
 	move_and_slide()
 
 func handle_gravity(delta: float):
 	velocity.y -= _GRAVITY * delta
 	if is_on_floor():
 		velocity.y = _JUMP_FORCE
+
+func handle_animation():
+	if velocity.y > 0: animation_player.play("jump")
+	else: animation_player.play("fall")
