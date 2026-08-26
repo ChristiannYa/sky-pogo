b19de5392c0f259c7cd30446ad746d39f5fc7d70
ChristiannYa <christian.ylopez02@gmail.com>
2026-08-26 19:28:12 -0400

    11-player-movement

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -7,6 +7,8 @@ extends CharacterBody3D
 
 const _JUMP_FORCE = 20.0
 const _GRAVITY = 40.0
+const _ROTATION_SPEED = 4.0
+const _MOVE_SPEED = 4.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,13 +16,27 @@ func _ready():
 
 func _physics_process(delta: float):
 	handle_gravity(delta)
-	handle_animation()
+	handle_rotation(delta)
+	handle_movement()
 	move_and_slide()
+	handle_animation()
 
 func handle_gravity(delta: float):
 	velocity.y -= _GRAVITY * delta
 	if is_on_floor():
 		velocity.y = _JUMP_FORCE
+
+func handle_rotation(delta: float):
+	var rotation: float = _ROTATION_SPEED * delta
+	if Input.is_action_pressed("ui_left"):
+		rotate_y(rotation)
+	if Input.is_action_pressed("ui_right"):
+		rotate_y(-rotation)
+
+func handle_movement():
+	var forward: Vector3 = transform.basis.z * Input.get_action_strength("ui_up")
+	velocity.x = forward.x * _MOVE_SPEED
+	velocity.z = forward.z * _MOVE_SPEED
 
 func handle_animation():
 	if velocity.y > 0: animation_player.play("jump")
