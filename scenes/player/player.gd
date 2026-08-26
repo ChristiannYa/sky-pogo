ff48361a4dbd7135543f766a16144b92c3539dbc
ChristiannYa <christian.ylopez02@gmail.com>
2026-08-26 17:50:15 -0400

    04-player-bounce

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,12 +1,19 @@
 class_name Player
 extends CharacterBody3D
 
-var _gv = ProjectSettings.get_setting("physics/3d/default_gravity")
+const JUMP_FORCE = 20.0
+
+var _gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-func _physics_process(delta: float) -> void:
-	velocity.y -= _gv * delta
+func _physics_process(delta: float):
+	handle_gravity(delta)
 	move_and_slide()
+
+func handle_gravity(delta: float):
+	velocity.y -= _gravity * delta
+	if is_on_floor():
+		velocity.y = JUMP_FORCE
