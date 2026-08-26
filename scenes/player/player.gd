49a710833204a9fcf446a4ba5a6dbcb7de5d6b15
ChristiannYa <christian.ylopez02@gmail.com>
2026-08-26 17:59:48 -0400

    06-platform-vanish

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,9 +1,8 @@
 class_name Player
 extends CharacterBody3D
 
-const JUMP_FORCE = 20.0
-
-var _gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+const _JUMP_FORCE = 20.0
+const _GRAVITY = 40.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,6 +13,6 @@ func _physics_process(delta: float):
 	move_and_slide()
 
 func handle_gravity(delta: float):
-	velocity.y -= _gravity * delta
+	velocity.y -= _GRAVITY * delta
 	if is_on_floor():
-		velocity.y = JUMP_FORCE
+		velocity.y = _JUMP_FORCE
