9bc8488d2a1c98eac126d491aafa31e82fdeb553
ChristiannYa <christian.ylopez02@gmail.com>
2026-08-26 18:47:37 -0400

    08-platform-hit

--- scenes/platform/platform.gd
+++ scenes/platform/platform.gd
@@ -1,7 +1,13 @@
 extends Node3D
 
 
+@export var wait_time = 4.0
+
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var timer: Timer = $Timer
+
+
+var _is_hit = false
 
 
 func _on_timer_timeout():
@@ -9,3 +15,8 @@ func _on_timer_timeout():
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "vanish": queue_free()
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body is Player and not _is_hit:
+		_is_hit = true
+		timer.start(wait_time * randf_range(0.75, 1.2))
