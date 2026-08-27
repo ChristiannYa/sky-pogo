66633b5f479b5bafe1383f095502bcf3204e0672
ChristiannYa <christian.ylopez02@gmail.com>
2026-08-27 19:03:21 -0400

    19-tree-pausing

--- game_ui/game_ui.gd
+++ game_ui/game_ui.gd
@@ -1,10 +1,19 @@
 extends Control
 
+
 @onready var game_over_color_rect: ColorRect = $GameOverColorRect
 
 
-func _ready() -> void:
+func _unhandled_input(event: InputEvent):
+	if event.is_action_pressed("reload"):
+		get_tree().reload_current_scene()
+
+func _enter_tree() -> void:
+	get_tree().paused = false
+
+func _ready():
 	SignalHub.game_over.connect(on_game_over)
 
 func on_game_over():
 	game_over_color_rect.show()
+	get_tree().paused = true
